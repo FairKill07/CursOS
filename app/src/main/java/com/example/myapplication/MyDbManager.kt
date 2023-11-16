37c28e14a538aa418c790460059af423dc132b12
Andrii53 <laggerfeedlag1992@gmail.com>
2023-11-16 15:39:36 +0200

    Add Db

---
+++ app/src/main/java/com/example/myapplication/MyDbManager.kt
@@ -0,0 +1,51 @@
+package com.example.myapplication
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+
+class MyDbManager (val context: Context) {
+    val myDbHelper = MyDbHelper(context);
+
+    var db: SQLiteDatabase? = null
+
+    fun openDb(){
+        db = myDbHelper.writableDatabase
+    }
+    fun insertToDb(title:String, path:String)
+    {
+        var values = ContentValues().apply {
+            put(MyDbNameClass.COLUMN_NAME_TITLE, title)
+            put(MyDbNameClass.COLUMN_NAME_PATH, path)
+        }
+        db?.insert(MyDbNameClass.TABLE_NAME, null, values)
+    }
+
+    @SuppressLint("Range")
+    fun readDb(): ArrayList<String>{
+        val db : SQLiteDatabase? = myDbHelper.writableDatabase
+        val dataList = ArrayList<String>();
+
+        val cursor = db?.query(MyDbNameClass.TABLE_NAME, null,null,null,null,null,null, null)
+
+        while (cursor?.moveToNext() !!)
+        {
+                val dataText = cursor?.getString(cursor.getColumnIndex(MyDbNameClass.COLUMN_NAME_TITLE))
+                val dataPath = cursor?.getString(cursor.getColumnIndex(MyDbNameClass.COLUMN_NAME_PATH))
+                Log.d("title", dataText.toString());
+                Log.d("path", dataPath.toString())
+                dataList.add(dataText.toString());
+        }
+
+
+        cursor?.close()
+        db.delete(MyDbNameClass.TABLE_NAME, null,null)
+        db.close();
+        return dataList
+    }
+
+
+}
